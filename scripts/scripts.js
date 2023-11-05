69d3535568f8f93cff7b9342c9f90fbc3bde8b72
KenCab123 <KennethCabral@pursuit.org>
2023-11-05 12:34:51 -0500

    Created my sneaker template function that creates a sneaker slot and i added the remove button to each as well as be able to change the color of the emoji based on the stock status

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ const sneakerCart = document.querySelector('.sneaker-cart');
 const closeButton = document.querySelector('#close');
 const listItems = document.querySelectorAll('.sneakers button');
 const reset = document.querySelector('#reset');
-const stockDropdown = document.querySelector('.sneakers ul');
+const stockDropdowns = document.querySelector('.sneakers ul');
 
 
 sneakerButton.addEventListener('click', () => {
@@ -13,3 +13,38 @@ sneakerButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     sneakerCart.classList.remove('open')
 })
+
+
+const sneakerTemplate = (name, price, stock, color, rating) => {
+    const li = document.createElement('li')
+    li.classList.add("single-sneaker");
+
+    const stockDropdown = document.createElement("select");
+    stockDropdown.innerHTML = `
+    <option value="In Stock" ${stock === 'In Stock' ? 'selected' : ''}>In Stock</option>
+    <option value="Out Of Stock" ${stock === 'Out Of Stock' ? 'selected' : ''}>Out Of Stock</option>
+    `;
+
+    stockDropdown.addEventListener('change', (event) => {
+        stock = event.target.value
+    });
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = 'Remove Sneaker';
+    removeButton.addEventListener('click', (event) => {
+        event.target.closest('.single-sneaker').remove();
+    });
+
+    li.innerHTML = `
+    <h3>${stock === 'In Stock' ? '🟢' : '🔴'}</h3>
+    <p><strong>Name</strong>: ${name}</p>
+    <p><strong>Price:</strong> $${price}</p>
+    <p><strong>${stockDropdown.outerHTML}</strong></p>
+    <p><strong>Color:</strong> ${color}</p>
+    <p><strong>Rating:</strong> ${rating}</p>
+   `;
+
+   li.appendChild(removeButton);
+
+   return li
+}
