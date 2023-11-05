9ae27c053812c56bbdc821bbe0c84764be1b09c3
KenCab123 <KennethCabral@pursuit.org>
2023-11-05 12:28:39 -0500

    Created cart functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,3 +4,12 @@ const closeButton = document.querySelector('#close');
 const listItems = document.querySelectorAll('.sneakers button');
 const reset = document.querySelector('#reset');
 const stockDropdown = document.querySelector('.sneakers ul');
+
+
+sneakerButton.addEventListener('click', () => {
+    sneakerCart.classList.toggle('open');
+})
+
+closeButton.addEventListener('click', () => {
+    sneakerCart.classList.remove('open')
+})
