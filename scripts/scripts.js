6f34b18e7b276cf91f9b355a27bf1adec6add9c7
KenCab123 <KennethCabral@pursuit.org>
2023-11-05 12:27:00 -0500

    Grabbed all the elements i need

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+const sneakerButton = document.querySelector('#add-sneaker');
+const sneakerCart = document.querySelector('.sneaker-cart');
+const closeButton = document.querySelector('#close');
+const listItems = document.querySelectorAll('.sneakers button');
+const reset = document.querySelector('#reset');
+const stockDropdown = document.querySelector('.sneakers ul');
