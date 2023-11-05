175b3af91af58283f64995c734cac0b8ae55e2c7
KenCab123 <KennethCabral@pursuit.org>
2023-11-05 12:53:02 -0500

    Added an event listener to the form that when submited it will clear the inputs, or reset the form.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
 const form = document.querySelector("form")
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const {name, price, stock, color, rating} = event.target;
+
+    generateSneaker(name.value, price.value, stock.value, color.value, rating.value);
+    
+    // NOT SURE WHY THE FORM.RESET() WAS NOT WORKING.
+    const sneakerAdder = event.target.closest('.sneaker-adder');
+    const textInputs = sneakerAdder.querySelectorAll('input[type="text"]');
+    
+    textInputs.forEach(input => {
+        input.value = ''; 
+    });
+
+    const numInputs = sneakerAdder.querySelectorAll('input[type="number"]');
+    
+    numInputs.forEach(input => {
+        input.value = ''; 
+    });
+
+    const dropDownInputs = sneakerAdder.querySelectorAll('select');
+    
+    dropDownInputs.forEach(input => {
+        input.value = "In Stock"; 
+    });
+})
